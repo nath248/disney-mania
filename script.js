8d73202bdc154ce48ad1b532d100dbd78f4e968f
natherrera248 <nmherrera20@outlook.com>
2021-11-11 13:41:54 -0500

    add classlist to elements and update to functions

--- script.js
+++ script.js
@@ -14,20 +14,32 @@ const movieBtn = document.querySelector("#random-movie-btn");
 const showBtn = document.querySelector("#random-show-btn");
 const searchForm = document.querySelector("#search-input");
 const searchInput = document.querySelector("#search");
+const movieDiv = document.querySelector("#movie");
+const showDiv = document.querySelector("#show");
+const titleDiv = document.querySelector("#title");
 
-async function fetchTitles(title) {
-  const BASE_URL = `${DOMAIN}/v1/list-titles/?apiKey=${API_KEY}&source_ids=${SOURCE_ID}`;
+async function fetchTitles() {
+  const TYPE = `movie,tv_series`;
+  const BASE_URL = `${DOMAIN}/v1/list-titles/?apiKey=${API_KEY}&source_ids=${SOURCE_ID}&types=${TYPE}&page=1`;
+  // const BASE_URL1 = `${DOMAIN}/v1/list-titles/?apiKey=${API_KEY}&source_ids=${SOURCE_ID}&types=${TYPE}&page=2`;
+  // const BASE_URL2 = `${DOMAIN}/v1/list-titles/?apiKey=${API_KEY}&source_ids=${SOURCE_ID}&types=${TYPE}&page=3`;
   try {
     let res = await axios.get(BASE_URL);
     const titles = res.data.titles;
+    // const titles1 = res[1].data.titles;
+    // const titles2 = res[2].data.titles;
     // console.log(titles);
+    // console.log(titles1);
+    // console.log(titles2);
     let title = searchInput.value;
     titles.forEach((titles) => {
       const titleValue = titles.title;
+      // const titleValue1 = titles1.title;
+      // const titleValue2 = titles2.title;
       if (titleValue == title) {
-        // console.log(titleValue);
         let searchId = titles.imdb_id;
-        fetchDetails(searchId);
+        console.log(searchId);
+        fetchTitleDetails(searchId);
       }
     });
   } catch (error) {
@@ -45,8 +57,8 @@ async function fetchMovies() {
     const titles = res.data.titles;
     const values = titles[Math.floor(Math.random() * titles.length)];
     const movieId = values.imdb_id;
-    console.log(movieId);
-    fetchDetails(movieId);
+    //console.log(movieId);
+    fetchMovieDetails(movieId);
   } catch (error) {
     console.log("ERROR!")
   } finally {
@@ -63,7 +75,7 @@ async function fetchShows() {
     const values = titles[Math.floor(Math.random() * titles.length)];
     const showId = values.imdb_id;
     // console.log(showId);
-    fetchDetails(showId);
+    fetchShowDetails(showId);
   } catch (error) {
     console.log("ERROR!")
   } finally {
@@ -71,32 +83,136 @@ async function fetchShows() {
   }
 }
 
-async function fetchDetails(id) {
+async function fetchMovieDetails(id) {
   const imdbId = id;
   const BASE_URL1 = `${DOMAIN1}/3/find/${imdbId}?api_key=${API_KEY1}&language=en-US&external_source=imdb_id`;
   try {
     let res = await axios.get(BASE_URL1);
     const disneyDetails = res.data;
-    if (TYPES === "movie") {
-      //Movie Details
-      let movieTitle = disneyDetails.movie_results[0].title;
-      let moviePoster = disneyDetails.movie_results[0].poster_path;
-      let moviePop = disneyDetails.movie_results[0].popularity;
-      let movieOverview = disneyDetails.movie_results[0].overview;
-      console.log(movieTitle);
-      console.log(moviePoster);
-      console.log(moviePop);
-      console.log(movieOverview);
-    } else if (TYPES === "tv_series") {
-      //Show Details
-      let showTitle = disneyDetails.tv_results[0].title;
-      let showPoster = disneyDetails.tv_results[0].poster_path;
-      let showPop = disneyDetails.tv_results[0].popularity;
-      let showOverview = disneyDetails.tv_results[0].overview;
-      console.log(showTitle);
-      console.log(showPoster);
-      console.log(showPop);
-      console.log(showOverview);
+    console.log(disneyDetails);
+    //Movie Details
+    let innerMovieDiv = document.createElement("div");
+    innerMovieDiv.classList.add("innerDiv");
+    movieDiv.append(innerMovieDiv);
+
+    let movieTitle = document.createElement("h2");
+    movieTitle.classList.add("title");
+    movieTitle.innerText = disneyDetails.movie_results[0].title;
+    innerMovieDiv.append(movieTitle);
+
+    let moviePoster = document.createElement("img");
+    moviePoster.src = `${DOMAIN1}${disneyDetails.movie_results[0].poster_path}`;
+    innerMovieDiv.append(moviePoster);
+
+    let movieRelease = document.createElement("p");
+    movieRelease.innerText = `Release Date: ${disneyDetails.movie_results[0].release_date}`;
+    innerMovieDiv.append(movieRelease);
+
+    let moviePop = document.createElement("p");
+    moviePop.innerText = `Popularity: ${disneyDetails.movie_results[0].popularity}%`;
+    innerMovieDiv.append(moviePop);
+
+    let movieOverview = document.createElement("p");
+    movieOverview.innerText = disneyDetails.movie_results[0].overview;
+    innerMovieDiv.append(movieOverview);
+  } catch (error) {
+    console.log(error)
+  } finally {
+    console.log("DONE!")
+  }
+}
+
+async function fetchShowDetails(id) {
+  const imdbId = id;
+  const BASE_URL1 = `${DOMAIN1}/3/find/${imdbId}?api_key=${API_KEY1}&language=en-US&external_source=imdb_id`;
+  try {
+    let res = await axios.get(BASE_URL1);
+    const disneyDetails = res.data;
+    console.log(disneyDetails);
+    //Show Details
+    let innerShowDiv = document.createElement("div");
+    innerShowDiv.classList.add("innerDiv");
+    showDiv.append(innerShowDiv);
+
+    let showTitle = document.createElement("h2");
+    showTitle.classList.add("title");
+    showTitle.innerText = disneyDetails.tv_results[0].name;
+    innerShowDiv.appendChild(showTitle);
+
+    let showPoster = document.createElement("img");
+    showTitle.src = `${DOMAIN1}${disneyDetails.tv_results[0].poster_path}`;
+    innerShowDiv.appendChild(showPoster);
+
+    let showRelease = document.createElement("p");
+    showRelease.innerText = `Air Date: ${disneyDetails.tv_results[0].first_air_date}`;
+    innerShowDiv.appendChild(showRelease);
+
+    let showPop = document.createElement("p");
+    showPop.innerText = `Popularity: ${disneyDetails.tv_results[0].popularity}`;
+    innerShowDiv.appendChild(showPop);
+
+    let showOverview = document.createElement("p");
+    showOverview.innerText = disneyDetails.tv_results[0].overview;
+    innerShowDiv.appendChild(showOverview);
+  } catch (error) {
+    console.log(error)
+  } finally {
+    console.log("DONE!")
+  }
+}
+
+async function fetchTitleDetails(id) {
+  const imdbId = id;
+  const BASE_URL1 = `${DOMAIN1}/3/find/${imdbId}?api_key=${API_KEY1}&language=en-US&external_source=imdb_id`;
+  try {
+    let res = await axios.get(BASE_URL1);
+    const disneyDetails = res.data;
+    //console.log(disneyDetails);
+    let innerTitleDiv = document.createElement("div");
+    innerTitleDiv.classList.add("innerDiv");
+    titleDiv.append(innerTitleDiv);
+    if (disneyDetails.movie_results.length === 1) {
+      let movieTitle = document.createElement("h2");
+      movieTitle.classList.add("title");
+      movieTitle.innerText = disneyDetails.movie_results[0].title;
+      innerTitleDiv.append(movieTitle);
+
+      let moviePoster = document.createElement("img");
+      moviePoster.src = `${DOMAIN1}${disneyDetails.movie_results[0].poster_path}`;
+      innerTitleDiv.append(moviePoster);
+
+      let movieRelease = document.createElement("p");
+      movieRelease.innerText = `Release Date: ${disneyDetails.movie_results[0].release_date}`;
+      innerTitleDiv.append(movieRelease);
+
+      let moviePop = document.createElement("p");
+      moviePop.innerText = `Popularity: ${disneyDetails.movie_results[0].popularity}`;
+      innerTitleDiv.append(moviePop);
+
+      let movieOverview = document.createElement("p");
+      movieOverview.innerText = disneyDetails.movie_results[0].overview;
+      innerTitleDiv.append(movieOverview);
+    } else if (disneyDetails.tv_results.length === 1) {
+      let showTitle = document.createElement("h2");
+      showTitle.classList.add("title");
+      showTitle.innerText = disneyDetails.tv_results[0].name;
+      innerTitleDiv.appendChild(showTitle);
+
+      let showPoster = document.createElement("img");
+      showTitle.src = `${DOMAIN1}${disneyDetails.tv_results[0].poster_path}`;
+      innerTitleDiv.appendChild(showPoster);
+
+      let showRelease = document.createElement("p");
+      showRelease.innerText = `Air Date: ${disneyDetails.tv_results[0].first_air_date}`;
+      innerTitleDiv.appendChild(showRelease);
+
+      let showPop = document.createElement("p");
+      showPop.innerText = `Popularity: ${disneyDetails.tv_results[0].popularity}`;
+      innerTitleDiv.appendChild(showPop);
+
+      let showOverview = document.createElement("p");
+      showOverview.innerText = disneyDetails.tv_results[0].overview;
+      innerTitleDiv.appendChild(showOverview);
     }
   } catch (error) {
     console.log(error)
@@ -105,11 +221,24 @@ async function fetchDetails(id) {
   }
 }
 
-// movieBtn.addEventListener("click", fetchMovies);
-// showBtn.addEventListener("click", fetchShows);
+movieBtn.addEventListener("click", () => {
+  removeDivContents();
+  fetchMovies();
+});
+showBtn.addEventListener("click", () => {
+  removeDivContents();
+  fetchShows();
+});
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  let title = searchInput.value;
-  fetchTitles(title);
-  // searchInput.value = "";
+  removeDivContents();
+  //searchInput.value = "";
+  fetchTitles();
+
 });
+
+function removeDivContents() {
+  movieDiv.innerHTML = "";
+  showDiv.innerHTML = "";
+  titleDiv.innerHTML = "";
+}
